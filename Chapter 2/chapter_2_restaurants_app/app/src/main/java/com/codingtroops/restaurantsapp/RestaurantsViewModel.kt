5b0912eb7e0da6441b50f8686968c37752a4fcc8
catalinghita8 <catalinghita8@gmail.com>
2021-10-19 15:37:22 +0300

    Add missing code for chapter 2

--- Chapter 2/chapter_2_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsViewModel.kt
+++ Chapter 2/chapter_2_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsViewModel.kt
@@ -1,17 +1,43 @@
 package com.codingtroops.restaurantsapp
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class RestaurantsViewModel() : ViewModel() {
-    val state = mutableStateOf(dummyRestaurants)
+
+class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewModel() {
+    val state = mutableStateOf(dummyRestaurants.restoreSelections())
 
     fun toggleFavorite(itemId: Int) {
         val restaurants = state.value.toMutableList()
         val itemIndex = restaurants.indexOfFirst { it.id == itemId }
         val item = restaurants[itemIndex]
         restaurants[itemIndex] = item.copy(isFavorite = !item.isFavorite)
+        storeSelection(restaurants[itemIndex])
         state.value = restaurants
+    }
+
+    private fun storeSelection(item: Restaurant) {
+        val savedToggled = stateHandle.get<List<Int>?>(FAVORITES)
+            .orEmpty().toMutableList()
+        if (item.isFavorite) savedToggled.add(item.id)
+        else savedToggled.remove(item.id)
+        stateHandle[FAVORITES] = savedToggled
+    }
+
+    private fun List<Restaurant>.restoreSelections(): List<Restaurant> {
+        stateHandle.get<List<Int>?>(FAVORITES)?.let { selectedIds ->
+            val restaurantsMap = this.associateBy { it.id }
+            selectedIds.forEach { id ->
+                restaurantsMap[id]?.isFavorite = true
+            }
+            return restaurantsMap.values.toList()
+        }
+        return this
+    }
+
+    companion object {
+        const val FAVORITES = "favorites"
     }
 
 }
