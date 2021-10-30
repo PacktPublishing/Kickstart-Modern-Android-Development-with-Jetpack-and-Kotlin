97cd29e245172171613668b9dd1036256a0fa172
catalinghita8 <catalinghita8@gmail.com>
2021-10-30 18:21:57 +0300

    Add final code for chapter 3

--- Chapter 3/chapter_3_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsScreen.kt
+++ Chapter 3/chapter_3_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsScreen.kt
@@ -10,12 +10,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
--- Chapter 3/chapter_3_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsViewModel.kt
+++ Chapter 3/chapter_3_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsViewModel.kt
@@ -1,12 +1,30 @@
 package com.codingtroops.restaurantsapp
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewModel() {
-    val state = mutableStateOf(dummyRestaurants.restoreSelections())
+    private var restInterface: RestaurantsApiService
+    private lateinit var restaurantsCall: Call<List<Restaurant>>
+    val state = mutableStateOf(emptyList<Restaurant>())
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://restaurants-db-default-rtdb.firebaseio.com/")
+            .build()
+        restInterface = retrofit.create(RestaurantsApiService::class.java)
+        getRestaurants()
+    }
 
     fun toggleFavorite(itemId: Int) {
         val restaurants = state.value.toMutableList()
@@ -25,6 +43,25 @@ class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewMode
         stateHandle[FAVORITES] = savedToggled
     }
 
+    private fun getRestaurants() {
+        restaurantsCall = restInterface.getRestaurants()
+        restaurantsCall.enqueue(
+            object : Callback<List<Restaurant>> {
+                override fun onResponse(
+                    call: Call<List<Restaurant>>,
+                    response: Response<List<Restaurant>>
+                ) {
+                    response.body()?.let { restaurants ->
+                        state.value = restaurants.restoreSelections()
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Restaurant>>, t: Throwable) {
+                    t.printStackTrace()
+                }
+            })
+    }
+
     private fun List<Restaurant>.restoreSelections(): List<Restaurant> {
         stateHandle.get<List<Int>?>(FAVORITES)?.let { selectedIds ->
             val restaurantsMap = this.associateBy { it.id }
@@ -34,6 +71,11 @@ class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewMode
             return restaurantsMap.values.toList()
         }
         return this
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        restaurantsCall.cancel()
     }
 
     companion object {
