bd9b4425d888cd68aa75dd24c4ca0122530cb87c
catalinghita8 <catalinghita8@gmail.com>
2021-12-22 16:25:53 +0200

    Encapsulate the UI state

--- Chapter 7/chapter_7_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsViewModel.kt
+++ Chapter 7/chapter_7_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsViewModel.kt
@@ -1,5 +1,6 @@
 package com.codingtroops.restaurantsapp
 
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,13 +11,15 @@ class RestaurantsViewModel() : ViewModel() {
 
     private val repository = RestaurantsRepository()
 
-    val state = mutableStateOf(RestaurantsScreenState(
+    private val _state = mutableStateOf(RestaurantsScreenState(
         restaurants = listOf(),
         isLoading = true))
+    val state: State<RestaurantsScreenState>
+        get() = _state
 
     private val errorHandler = CoroutineExceptionHandler { _, exception ->
         exception.printStackTrace()
-        state.value = state.value.copy(error = exception.message, isLoading = false)
+        _state.value = _state.value.copy(error = exception.message, isLoading = false)
     }
 
     init {
@@ -27,14 +30,14 @@ class RestaurantsViewModel() : ViewModel() {
         viewModelScope.launch(errorHandler) {
             val updatedRestaurants =
                 repository.toggleFavoriteRestaurant(itemId, oldValue)
-            state.value = state.value.copy(restaurants = updatedRestaurants)
+            _state.value = _state.value.copy(restaurants = updatedRestaurants)
         }
     }
 
     private fun getRestaurants() {
         viewModelScope.launch(errorHandler) {
             val restaurants = repository.getAllRestaurants()
-            state.value = state.value.copy(
+            _state.value = _state.value.copy(
                 restaurants = restaurants,
                 isLoading = false)
         }
