3a830ed7b100b0ffaf021dea3404dfec3fd9cfa4
catalinghita8 <catalinghita8@gmail.com>
2022-01-10 14:08:36 +0200

    Add Chapter corrections for build to correctly run

--- Chapter 8/chapter_8_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/restaurants/domain/IRestaurantsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.codingtroops.restaurantsapp.restaurants.domain
-
-interface IRestaurantsRepository {
-    suspend fun loadRestaurants()
-    suspend fun getRestaurants(): List<Restaurant>
-    suspend fun toggleFavoriteRestaurant(id: Int, value: Boolean)
-}
