1f30079c77e27f6048038a87bb0a8da46236ab49
catalinghita8 <catalinghita8@gmail.com>
2022-04-08 23:42:32 +0300

    Update Ch09 source code to Compose to 1.1.1, Kotlin to 1.6.10 and core-ktx, appcompat

--- Chapter_09/chapter_9_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsApplication.kt
+++ Chapter_09/chapter_9_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsApplication.kt
@@ -1,7 +1,6 @@
 package com.codingtroops.restaurantsapp
 
 import android.app.Application
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
--- Chapter_09/chapter_9_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/restaurants/presentation/MainActivity.kt
+++ Chapter_09/chapter_9_restaurants_app/app/src/main/java/com/codingtroops/restaurantsapp/restaurants/presentation/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
