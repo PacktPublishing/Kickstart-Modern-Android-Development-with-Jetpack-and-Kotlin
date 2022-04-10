c00fdd1af3e95a1e7e9b35026e811e89f0ae9acf
catalinghita8 <catalinghita8@gmail.com>
2022-04-10 13:57:31 +0300

    Update Ch11 solution source code to Compose to 1.1.1, Kotlin to 1.6.10 and core-ktx, appcompat

--- Chapter_11/repositories_app_solution_ch11/app/src/main/java/com/codingtroops/repositories/RepositoriesScreen.kt
+++ Chapter_11/repositories_app_solution_ch11/app/src/main/java/com/codingtroops/repositories/RepositoriesScreen.kt
@@ -2,7 +2,6 @@ package com.codingtroops.repositories
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
